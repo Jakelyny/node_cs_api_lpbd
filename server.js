39f4ec72c491573d3864632e95f4da231c3ea222
Jakelyny <jakelynysousa.a@gmail.com>
2021-09-21 21:11:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ front_end : responsável pela interação com o usuário (interface gráfica)
 Requisitos para o funcionamento:
 Node, npm, express e pg
 
-Comandos para a isntalação dos pacotes:
+Comandos para a instalação dos pacotes:
 npm install --save express
 npm install --save pg
 */
 
 var express = require('express'); //requisita a biblioteca para a criação dos serviçoes web.
+var pg = require("pg");
 
 var sw = express(); // inicializa uma variável chamada app que possibilitará a criação dos serviços e rotas.
 
@@ -26,6 +27,34 @@ sw.use(function(req, res, next){
     next();
 });
 
+const config= {
+    host: 'localhost',
+    user: 'postgres',
+    database: 'db_cs_lpbd_2021_9',
+    password: '123456',
+    port: 5432
+}
+
+//definida a conexão com o banco de dados.
+const postgres = new pg.Pool(config);
+
+//definindo um serviço web, que estará acessível pelo endereço http://localhost:400/listjogador
+sw.get('/listjogador', function (req, res){
+
+    postgres.connect(function(err,client,done){
+
+        if(err){
+
+            console.log("Não conseguiu acessar o BD :" + err);
+            res.status(400).send('{'+err+'}');
+        }else{
+
+            client.query('SELECT j.nickname, to_char(j.data_nascimento, ')
+        }
+    })
+
+});
+
 //definição do primeiro serviço web.
 sw.get('/', (req, res) => {
     res.send('HELLOO, WORLD! Olá, eu sou um teste ! ! !');
@@ -35,3 +64,4 @@ sw.get('/', (req, res) => {
 sw.listen(4000, function(){
     console.log('Server is running.. on Port 4000');
 });
+
