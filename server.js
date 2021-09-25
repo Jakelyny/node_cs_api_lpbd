2566bf3d71b1eace1c5d116236bc62405e2abda6
Jakelyny <jakelynysousa.a@gmail.com>
2021-09-24 21:06:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,55 +13,186 @@ npm install --save express
 npm install --save pg
 */
 
-var express = require('express'); //requisita a biblioteca para a criação dos serviçoes web.
-var pg = require("pg");
+var express = require('express'); // requisita a biblioteca para a criacao dos serviços web.
+var pg = require("pg"); // requisita a biblioteca pg para a comunicacao com o banco de dados.
 
-var sw = express(); // inicializa uma variável chamada app que possibilitará a criação dos serviços e rotas.
+var sw = express(); // iniciliaza uma variavel chamada app que possitilitará a criação dos serviços e rotas.
 
-sw.use(express.json()); //padrão de mensagem em json
+sw.use(express.json());//padrao de mensagens em json.
 
-sw.use(function(req, res, next){
-    res.header('Acess-Control-Allow-Origin', '*');
-    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Acess-Control-Allow-Methods', 'GET,POST');
-    next();
+sw.use(function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET,POST');
+   next();
 });
 
-const config= {
-    host: 'localhost',
-    user: 'postgres',
-    database: 'db_cs_lpbd_2021_9',
-    password: '123456',
-    port: 5432
-}
+const config = {
+   host: 'localhost',
+   user: 'postgres',
+   database: 'db_cs_lpbd_2020_2',
+   password: '123456',
+   port: 5432
+};
 
-//definida a conexão com o banco de dados.
+//definia conexao com o banco de dados.
 const postgres = new pg.Pool(config);
 
-//definindo um serviço web, que estará acessível pelo endereço http://localhost:400/listjogador
-sw.get('/listjogador', function (req, res){
+sw.get('/listpatente', function (req, res) {
 
-    postgres.connect(function(err,client,done){
+   postgres.connect(function(err,client,done) {
 
-        if(err){
+      if(err){
 
-            console.log("Não conseguiu acessar o BD :" + err);
-            res.status(400).send('{'+err+'}');
-        }else{
+          console.log("Não conseguiu acessar o BD :"+ err);
+          res.status(400).send('{'+err+'}');
+      }else{
+       client.query('SELECT p.codigo, p.nome FROM tb_patente p order by p.codigo asc',function(err,result) {        
+               done(); // closing the connection;
+               if(err){
+                   console.log(err);
+                   res.status(400).send('{'+err+'}');
+               }else{
+                   res.status(200).send(result.rows);
+               }
+               
+           });
+      } 
+   });
+});
 
-            client.query('SELECT j.nickname, to_char(j.data_nascimento, ')
-        }
-    })
+
+//definindo um serviço web, que estará acessível pelo endereço http://localhost:4000/listjogador
+sw.get('/listjogador', function (req, res) {
+
+   postgres.connect(function(err,client,done) {
+
+      if(err){
+
+          console.log("Não conseguiu acessar o BD :"+ err);
+          res.status(400).send('{'+err+'}');
+      }else{
+       
+       client.query('SELECT j.nickname, to_char(j.data_nascimento, \'yyyy-mm-dd\') as data_nascimento,  j.qtd_estrela, p.codigo, p.nome, p.descricao, j.senha, to_char(j.data_cadastro, \'yyyy-mm-dd\') as data_cadastro FROM tb_jogador j, tb_patente p where j.patente_codigo=p.codigo order by j.data_cadastro asc',function(err,result) {        
+               
+               done(); // closing the connection;
+               if(err){
+
+                   console.log(err);
+
+                   res.status(400).send('{'+err+'}');
+
+               }else{
+
+                   res.status(200).send(result.rows);//retorna para o requisitante a lista de registro(s) encontrado(s) pelo comando select.
+               }
+               
+           });
+      } 
+   });
+});
+
+sw.post('/insertjogador', function (req, res, next) {
+   
+   postgres.connect(function(err,client,done) {
+
+      if(err){
+
+          console.log("Nao conseguiu acessar o  BD "+ err);
+          res.status(400).send('{'+err+'}');
+      }else{            
+
+           var q ={
+               text: 'insert into tb_jogador (nickname, data_nascimento, senha, patente_codigo, qtd_estrela, data_cadastro) values ($1,$2,$3,$4,$5, now())',
+               values: [req.body.nickname, req.body.data_nascimento, req.body.senha, req.body.patente.codigo, req.body.qtd_estrela]
+           }
+           console.log(q);
+   
+           client.query(q,function(err,result) {
+               done(); // closing the connection;
+               if(err){
+                   console.log('retornou 400 no insert');
+                   console.log(err);
+                   console.log(err.data);
+                   res.status(400).send('{'+err+'}');
+               }else{
+
+                   console.log('retornou 201 no insert');
+                   res.status(201).send(req.body.nickname);//se não realizar o send nao finaliza o client
+               }           
+           });
+      }       
+   });
+});
+
+sw.post('/updatejogador/', (req, res) => {
+
+   postgres.connect(function(err,client,done) {
+       if(err){
+
+           console.log("Não conseguiu acessar o BD: "+ err);
+           res.status(400).send('{'+err+'}');
+
+       }else{
+
+           var q ={
+               text: 'update tb_jogador set data_nascimento = $1, senha = $2, qtd_estrela = $3, patente_codigo = $4 where nickname = $5',
+               values: [req.body.data_nascimento, req.body.senha, req.body.qtd_estrela, req.body.patente.codigo, req.body.nickname]
+           }
+           console.log(q);
+    
+           client.query(q,function(err,result) {
+               done(); // closing the connection;
+               if(err){
+                   console.log("Erro no updatejogador: "+err);
+                   res.status(400).send('{'+err+'}');
+               }else{             
+                   res.status(200).send(req.body.nickname);//se não realizar o send nao finaliza o client nao finaliza
+               }
+           });
+       }
+    });
+});
+
+
+
+
+sw.get('/deletejogador/:nickname', (req, res) => {
+
+   postgres.connect(function(err,client,done) {
+       if(err){
+           console.log("Não conseguiu acessar o serviço deletejogador!"+ err);
+           res.status(400).send('{'+err+'}');
+       }else{
+           
+           var q ={
+               text: 'delete FROM tb_jogador where nickname = $1',
+               values: [req.params.nickname]
+           }
+   
+           client.query( q , function(err,result) {
+               done(); // closing the connection;
+               if(err){
+                   console.log(err);
+                   res.status(400).send('{'+err+'}');
+               }else{
+                   res.status(200).send({'nickname': req.params.nickname});//retorna o nickname deletado.
+               }
+
+           });
+       } 
+    });
 
 });
 
-//definição do primeiro serviço web.
+
+
+//definicao do primeiro serviço web.
 sw.get('/', (req, res) => {
-    res.send('HELLOO, WORLD! Olá, eu sou um teste ! ! !');
-});
+   res.send('Hello, world! meu primeiro teste.  #####');
+})
 
-//definição da porta
-sw.listen(4000, function(){
-    console.log('Server is running.. on Port 4000');
-});
 
+sw.listen(4000, function () {
+   console.log('Server is running.. on Port 4000');
+});
